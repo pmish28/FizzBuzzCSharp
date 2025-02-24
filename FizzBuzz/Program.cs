d5e63de0c30828b58193ed88991e1bca9d384a26
Preeti Mishra <preeti.mishra@softwire.com>
2025-02-24 12:30:50 +0000

    Add code to print fizz for multiple of 3 buzz for multiple of 5 and fizzbuzz for multiple of 5

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FizzBuzz
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FizzBuzz
 {
     public class FizzBuzz
     {
@@ -9,13 +11,28 @@
         }
         public static void PrintNumbers()
         {
-            for(int num =1; num<=100; num++)
+            for (int num = 1; num <= 100; num++)
             {
-                Console.WriteLine(num);
+                switch (num % 15)
+                {
+
+                    case 0:
+                        Console.WriteLine("FizzBuzz");
+                        break;
+                    case 3:
+                        Console.WriteLine("Fizz");
+                        break;
+                    case 5:
+                        Console.WriteLine("Buzz");
+                        break;
+                    default:
+                        Console.WriteLine(num);
+                        break;
+                }
+
             }
         }
     }
-    
+
 }
-    
- 
+
